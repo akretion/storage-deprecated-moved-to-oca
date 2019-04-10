68bb000ba91630a920a7ee25d2fe4d87f6455005
Benoit <benoit.guillot@akretion.com>
2019-04-10 17:26:23 +0200

    [FIX] js

--- storage_image_product_pos/static/src/js/pos_product.js
+++ storage_image_product_pos/static/src/js/pos_product.js
@@ -4,11 +4,12 @@
     License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 */
 
-odoo.define("storage_image_product.pos_product", function(require){
+odoo.define("storage_image_product.pos_product", function (require) {
     "use strict";
 
     var screens = require("point_of_sale.screens");
     var models = require('point_of_sale.models');
+
     /* ********************************************************
     Overload: point_of_sale.ProductListWidget
 
@@ -16,7 +17,8 @@ odoo.define("storage_image_product.pos_product", function(require){
     - display product custom storage image;
     *********************************************************** */
     screens.ProductListWidget.include({
-        get_product_image_url: function(product) {
+        get_product_image_url: function (product) {
+
            /* ************************************************
            Overload: 'get_product_image_url'
            */
@@ -24,17 +26,16 @@ odoo.define("storage_image_product.pos_product", function(require){
         },
     });
 
-     /*********************************************************
+    /* ********************************************************
     Overload: point_of_sale.PosModel
     - Overload module.PosModel.initialize function to load extra-data
          - Load 'image_medium_url' field of model product.product;
     *********************************************************** */
-    // change product.product call
     models.PosModel.prototype.models.some(function (model) {
         if (model.model !== 'product.product') {
             return false;
         }
-        if (model.fields.indexOf('image_medium_url') == -1) {
+        if (model.fields.indexOf('image_medium_url') ===  -1) {
             model.fields.push('image_medium_url');
         }
     });
